104609d9a3a071c1442984937f5ee240f522c2d2
Samuel <samuelrivba@gmail.com>
2024-04-11 19:17:52 -0500

    Add ES6 modules

--- modules.js
+++ modules.js
@@ -40,3 +40,11 @@ console.log(fightModule.fight("ron", "harry"));
 //     dance: dance,
 //   };
 // });
+
+// Native modules
+
+export function fight2(char1, char2) {
+  const attack1 = Math.floor(Math.random() * char1.length);
+  const attack2 = Math.floor(Math.random() * char2.length);
+  return attack1 > attack2 ? `${char1} wins` : `${char2} wins`;
+}
